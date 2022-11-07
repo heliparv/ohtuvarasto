3f8e99126ef2c3e2a1cc4bc1d204004e3b3bb810
Heli Parviainen <heli.s.parviainen@helsinki.fi>
2022-11-07 19:14:18 +0200

    testikattavuus sataseen

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -38,3 +38,45 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+    
+    def test_tilavuus_ali_nolla(self):
+        self.varasto = Varasto(-10)
+        self.assertAlmostEqual(self.varasto.tilavuus, 0)
+    
+    def test_saldo_nolla_jos_annettu(self):
+        self.varasto = Varasto(10, 0)
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+    
+    def test_saldo_nolla_jos_annettu_negatiivinen(self):
+        self.varasto = Varasto(10, -10)
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+
+    def test_lisatty_maara_pienempi_kuin_nolla(self):
+        self.varasto.lisaa_varastoon(-5)
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+    
+    def test_lisatty_maara_ei_mahdu(self):
+        self.varasto.lisaa_varastoon(555)
+        self.assertAlmostEqual(self.varasto.saldo, self.varasto.tilavuus)
+    
+    def test_varastosta_ei_oteta_jos_saldo_nolla(self):
+        annettu = self.varasto.ota_varastosta(7)
+        self.assertAlmostEqual(annettu, 0)
+    
+    def test_varastosta_ei_oteta_negatiivista_summaa(self):
+        self.varasto.lisaa_varastoon(5)
+        annettu = self.varasto.ota_varastosta(-7)
+        self.assertAlmostEqual(annettu, 0)
+    
+    def test_varastosta_ei_oteta_liikaa(self):
+        self.varasto.lisaa_varastoon(5)
+        annettu = self.varasto.ota_varastosta(7)
+        self.assertAlmostEqual(annettu, 5)
+    
+    def test_varaston_saa_otettua_tyhjaksi(self):
+        self.varasto.lisaa_varastoon(5)
+        annettu = self.varasto.ota_varastosta(7)
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+    
+    def test_merkkijonotulostus_toimii(self):
+        self.assertEqual(str(self.varasto), "saldo = 0, vielä tilaa 10")
