9f22fa9cfc6cc6ede8de4c9f4e6a17b5c62a74e0
Heli Parviainen <heli.s.parviainen@helsinki.fi>
2022-11-07 19:23:27 +0200

    korjasin testin

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -79,4 +79,4 @@ class TestVarasto(unittest.TestCase):
         self.assertAlmostEqual(self.varasto.saldo, 0)
     
     def test_merkkijonotulostus_toimii(self):
-        self.assertEqual(str(self.varasto), "saldo = 0, vielä tilaa 10.0")
+        self.assertEqual(str(self.varasto), "saldo = 0, vielä tilaa 10")
